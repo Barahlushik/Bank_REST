6e8e48830b57af290f0427ba61c0349289dd0a02
RotoScoping <barahlush@melchior.localdomain>
2025-09-18 15:43:32 +0300

    feat(liquibase): add new migration scripts (008-create-card-block.yaml) for schema changes

--- src/main/java/com/example/bankcards/Application.java
+++ src/main/java/com/example/bankcards/Application.java
@@ -1,8 +1,11 @@
 package com.example.bankcards;
 
+import com.example.bankcards.config.properties.SecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(SecurityProperties.class)
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
